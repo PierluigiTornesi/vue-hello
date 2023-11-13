44809350f09415b6be976eebd282640e448d853c
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-11-13 15:09:39 +0100

    add: bonus foto

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const {createApp} = Vue;
 const app = createApp({
     data(){
         return{
-           greeting: "ciao utente",
+           greeting: "ciao utente, questa é una foto di un lago di notte con la luna",
+           imgSrc:"https://c4.wallpaperflare.com/wallpaper/297/22/531/lake-blue-moonlight-moon-wallpaper-preview.jpg",
         };
     },
     methods: {
